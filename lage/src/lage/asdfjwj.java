b7d9ffa919b581a2102862036950e8b62163f2b6
Student <Student@EEIT-PC>
2016-02-03 16:50:06 +0800

    WDAWDAWDAWDAWDAWDAWDAWDAWDAWDAWDAWD

--- lage/src/lage/asdfjwj.java
+++ lage/src/lage/asdfjwj.java
@@ -4,6 +4,11 @@ public class asdfjwj {
 
 	public static void main(String[] args) {
 		System.out.println("WADAWDAWDAW");
+		System.out.println("WADAWDAWDAW");
+		System.out.println("WADAWDAWDAW");
+		System.out.println("WADAWDAWDAW");
+		System.out.println("WADAWDAWDAW");
+		
 	}
 
 }
